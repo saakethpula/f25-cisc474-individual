91338710252d10e5ffe1723fde2a1ef5338cefb8
Saaketh <saakethp@udel.edu>
2025-10-03 13:01:11 -0400

    pushing cors policy updates to backend

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,10 +3,17 @@ import { AppModule } from './app.module';
 
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   const host = process.env.HOST || undefined;
+  app.enableCors({
+    origin: ['https://f25-cisc474-individual-234i.onrender.com', 'http://localhost:3000', 'http://localhost:3001'],
+    credentials: true,
+  });
+
   await app.listen(port, host);
+  
 }
 
 void bootstrap();
