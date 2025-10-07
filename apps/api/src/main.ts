65bdc19b4e9bd702a8b6fb5c307e0b38828d2eb9
Saaketh <saakethp@udel.edu>
2025-10-07 15:35:31 -0400

    added new frontend link

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   const host = process.env.HOST || undefined;
   app.enableCors({
-    origin: ['https://f25-cisc474-individual-web-chi.vercel.app', 'https://f25-cisc474-individual-8gs54983q-saakethpulas-projects.vercel.app', 'https://f25-cisc474-individual-234i.onrender.com', 'http://localhost:3000', 'http://localhost:3001'],
+    origin: ['https://474-individual-web-app.saakethp.workers.dev', 'https://f25-cisc474-individual-8gs54983q-saakethpulas-projects.vercel.app', 'https://f25-cisc474-individual-234i.onrender.com', 'http://localhost:3000', 'http://localhost:3001'],
     credentials: true,
   });
 
