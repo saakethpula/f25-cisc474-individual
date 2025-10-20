63d2fbbe976dacd890f2e5aa349422e21e255950
Saaketh <saakethp@udel.edu>
2025-10-19 22:28:50 -0400

    updating dto ;

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,14 +2,16 @@ import { Link } from './links/entities/link.entity';
 
 import { CreateLinkDto } from './links/dto/create-link.dto';
 import { UpdateLinkDto } from './links/dto/update-link.dto';
-import { UpdateMessageDto } from './message/dto/update-message.dto';
+import type { UpdateMessageDto } from './message/dto/update-message.dto';
 
 export const links = {
   dto: {
     CreateLinkDto,
     UpdateLinkDto,
-    UpdateMessageDto,
   },
+  message: {
+    // types only
+  } as { UpdateMessageDto?: UpdateMessageDto },
   entities: {
     Link,
   },
